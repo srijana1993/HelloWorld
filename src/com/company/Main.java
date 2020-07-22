835bfa8fe458789cee70beeb4f341e7824f3164a
srijana1993 <59325270+srijana1993@users.noreply.github.com>
2020-07-22 12:17:37 -0500

    added current date and time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world ! from github.com.");
         System.out.print("Srijana Timalsina");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
 
     }
 }
